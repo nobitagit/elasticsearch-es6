0b06ae0702fa947e81a6697a0c2ff519658db4bd
Nobita <aurelio.ogliari@gmail.com>
2016-02-15 23:01:14 +0000

    indent fix

--- server/elastic.js
+++ server/elastic.js
@@ -1,8 +1,8 @@
 import es from 'elasticsearch';
 
 var esClient = new es.Client({
-    host: 'localhost:9200',
-    log: 'info'
+  host: 'localhost:9200',
+  log: 'info'
 });
 
 var indexName = "randomindex";
@@ -11,76 +11,76 @@ var indexName = "randomindex";
 * Delete an existing index
 */
 export let deleteIndex = () => {
-    return esClient.indices.delete({
-        index: indexName
-    });
+  return esClient.indices.delete({
+    index: indexName
+  });
 }
 
 /**
 * create the index
 */
 export let initIndex = () => {
-    return esClient.indices.create({
-        index: indexName
-    });
+  return esClient.indices.create({
+    index: indexName
+  });
 }
 
 /**
 * check if the index exists
 */
 export let indexExists = () => {
-    return esClient.indices.exists({
-        index: indexName
-    });
+  return esClient.indices.exists({
+    index: indexName
+  });
 }
 
 export let initMapping = () => {
-    return esClient.indices.putMapping({
-        index: indexName,
-        type: "document",
-        body: {
-            properties: {
-                title: { type: "string" },
-                content: { type: "string" },
-                suggest: {
-                    type: "completion",
-                    analyzer: "simple",
-                    search_analyzer: "simple",
-                    payloads: true
-                }
-            }
+  return esClient.indices.putMapping({
+    index: indexName,
+    type: "document",
+    body: {
+      properties: {
+        title: { type: "string" },
+        content: { type: "string" },
+        suggest: {
+          type: "completion",
+          analyzer: "simple",
+          search_analyzer: "simple",
+          payloads: true
         }
-    });
+      }
+    }
+  });
 }
 
 export let addDocument = (doc) => {
-    return esClient.index({
-        index: indexName,
-        type: "document",
-        body: {
-            title: doc.title,
-            content: doc.content,
-            suggest: {
-                input: doc.title.split(' ').concat(doc.title),
-                output: doc.title,
-                payload: doc.metadata || {}
-            }
-        }
-    });
+  return esClient.index({
+    index: indexName,
+    type: "document",
+    body: {
+      title: doc.title,
+      content: doc.content,
+      suggest: {
+        input: doc.title.split(' ').concat(doc.title),
+        output: doc.title,
+        payload: doc.metadata || {}
+      }
+    }
+  });
 }
 
 export let getSuggestions = (input) => {
-    return esClient.suggest({
-        index: indexName,
-        type: "document",
-        body: {
-            docsuggest: {
-                text: input,
-                completion: {
-                    field: "suggest",
-                    fuzzy: true
-                }
-            }
+  return esClient.suggest({
+    index: indexName,
+    type: "document",
+    body: {
+      docsuggest: {
+        text: input,
+        completion: {
+          field: "suggest",
+          fuzzy: true
         }
-    });
+      }
+  }
+});
 }
--- server/index.js
+++ server/index.js
@@ -19,59 +19,58 @@ app.get('/', function (req, res) {
 
 app.get('/suggest/:input', function (req, res, next) {
   es.getSuggestions(req.params.input)
-  	.then((result) => res.json(result) );
+    .then((result) => res.json(result) );
 });
 
 /* POST document to be indexed */
 app.post('/addBook', function (req, res, next) {
 
-	es.addDocument({
-		title: req.body.title,
-		content: req.body.content
-	})
-	.then(data => res.status(200).json(data))
-	.catch(e => res.status(500));
+  es.addDocument({
+    title: req.body.title,
+    content: req.body.content
+  })
+  .then(data => res.status(200).json(data))
+  .catch(e => res.status(500));
 
 });
 
 app.get('/removeIndex',(req, res) => {
-	es.indexExists().then(function(exists){
-		if (!exists) {
-			return res.status(200).json({
-				message: "index does not exist"
-			})
-		} else {
-			es.deleteIndex().then(()=>{
-				res.status(200).json({
-					message: 'deleted'
-				});
-			});
-		}
-	});
-
+  es.indexExists().then(function(exists){
+    if (!exists) {
+      return res.status(200).json({
+        message: "index does not exist"
+      })
+    } else {
+      es.deleteIndex().then(()=>{
+        res.status(200).json({
+          message: 'deleted'
+        });
+      });
+    }
+  });
 });
 
 app.get('/createIndex',(req, res) => {
 
-	es.indexExists().then(function(exists){
-		if (exists) {
-			return res.status(200).json({
-				message: "index already exists"
-			})
-		} else {
-			es.initIndex()
-				.then(es.initMapping)
-				.then((mapping)=>{
-					return res.status(200).json({
-						message: 'created'
-					});
-			});
-		}
-	});
+  es.indexExists().then(function(exists){
+    if (exists) {
+      return res.status(200).json({
+        message: "index already exists"
+      })
+    } else {
+      es.initIndex()
+        .then(es.initMapping)
+        .then((mapping)=>{
+          return res.status(200).json({
+            message: 'created'
+          });
+      });
+    }
+  });
 });
 
 app.listen(port, function () {
-	console.log(`Listening. Open http://localhost:${port}`)
+  console.log(`Listening. Open http://localhost:${port}`)
 });
 
 //export default app;
